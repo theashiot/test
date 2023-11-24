d19f7785d306f08739c635d6149b4e2099c8920a
Ashwin Mehendale <amehenda@amehenda.pnq.csb>
2023-11-24 18:41:59 +0530

    fix pom

--- helloworld/src/test/java/org/jboss/as/quickstarts/helloworld/HelloWorldServletIT.java
+++ helloworld/src/test/java/org/jboss/as/quickstarts/helloworld/HelloWorldServletIT.java
@@ -20,6 +20,7 @@ public class HelloWorldServletIT {
         if (serverHost == null) {
             serverHost = DEFAULT_SERVER_HOST;
         }
+	System.out.println (serverHost);
         final HttpRequest request = HttpRequest.newBuilder()
                 .uri(new URI(serverHost+"/HelloWorld"))
                 .GET()
